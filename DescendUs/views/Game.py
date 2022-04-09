1ef421305229cc5b9e93105213b2359cc0d9a8aa
Jason <jasonli.0616@outlook.com>
2022-04-09 11:45:04 -0400

    Create earth, create win game screen

--- DescendUs/views/Game.py
+++ DescendUs/views/Game.py
@@ -44,8 +44,18 @@ class Game:
             the surface to render the components onto
         """
 
+        # Display Earth
+        if not _globals.Earth.earth:
+            _globals.Earth.earth = objects.Earth()
+
+        earth = _globals.Earth.earth
+        earth.draw(surface)
+
         # Display player
-        self.player = objects.Player(surface, self.rect)
+        if not _globals.Game.player:
+            _globals.Game.player = objects.Player(surface, self.rect)
+
+        self.player = _globals.Game.player
         self.player.draw()
 
         # Display lasers
@@ -68,6 +78,11 @@ class Game:
         # Display collidables
         for collidable in _globals.Game.collidables:
             collidable.draw(surface)
+
+        # Display win screen
+        if _globals.Game.won:
+            win_image = pygame.image.load(os.path.join(_globals.Game.ASSETS_DIR, 'win_screen.png'))
+            surface.blit(win_image, ( 0 + (surface.get_width() - win_image.get_width())/2, 400 ))
 
 
     def _handle_event(self):
@@ -110,9 +125,8 @@ class Game:
         # Descend at regular pace
         _globals.Game.km_to_earth -= 1 / _globals.Window.FPS
 
-        if _globals.Game.km_to_earth < 0:
-            # Handle game win
-            pass
+        if _globals.Game.km_to_earth <= 0:
+            self.win()
 
 
     def _generate_collidable(self):
@@ -124,9 +138,22 @@ class Game:
             1 in 100 - laser
 
         Random chance once per frame (60 times per second).
+
+        Does not generate if the player is close to Earth (20 km).
         """
 
-        if random.randint(0, 50) == 1:
-            return objects.Collidable.Asteroid( (random.randint(0, _globals.Window.WIDTH), _globals.Window.HEIGHT) )
-        elif random.randint(0, 100) == 1:
-            return objects.Collidable.Ammo( (random.randint(0, _globals.Window.WIDTH), _globals.Window.HEIGHT) )
+        if _globals.Game.km_to_earth > 20:
+
+            if random.randint(0, 50) == 1:
+                return objects.Collidable.Asteroid( (random.randint(0, _globals.Window.WIDTH), _globals.Window.HEIGHT) )
+            elif random.randint(0, 100) == 1:
+                return objects.Collidable.Ammo( (random.randint(0, _globals.Window.WIDTH), _globals.Window.HEIGHT) )
+
+
+    def win(self):
+        """
+        Handle game win.
+        """
+
+        _globals.Game.km_to_earth = 0
+        _globals.Game.won = True
